535e2b4d19e6e7863e1bec2a35031100ae96e050
Tucker Melssen <tuckerjmelssen@gmail.com>
2024-02-01 13:39:08 -0600

    boilerplate for shelf.router setup, next add queryText/params

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -6,14 +6,37 @@ const router = express.Router();
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+  // initialize queryText
+  const queryText = ``
+  // send query to DB
+  pool.query(queryText)
+  .then(result => {
+    console.log("result", result)
+    // send result in rows
+    res.send(result.rows)
+  })
+  .catch(err => {
+    console.log('Error getting items:', err)
+    res.sendStatus(500)
+  })
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
 router.post('/', (req, res) => {
-  // endpoint functionality
+  // initialize queryText
+  const queryText = ``
+  const queryParams = ``
+  // send POST query and queryParams to DB
+  pool.query(queryText, queryParams)
+  .then(result => {
+    res.sendStatus(201)
+  })
+  .catch(err => {
+    console.error('Erroring posting item:', err)
+    res.sendStatus(500)
+  })
 });
 
 /**
@@ -21,6 +44,15 @@ router.post('/', (req, res) => {
  */
 router.delete('/:id', (req, res) => {
   // endpoint functionality
+  const queryText = ``
+  const queryParams = ``
+  pool.query(queryText, queryParams)
+  .then(result => {
+    res.sendStatus(201)
+  })
+  .catch(err => {
+    console.error('Error deleting item:', err)
+  })
 });
 
 /**
